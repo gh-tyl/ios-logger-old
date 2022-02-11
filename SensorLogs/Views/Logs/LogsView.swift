d0d2044309f559a9bab2a0b4a2ab351ddfe3f62f
Tyler-Inari <s1922054@stu.musashino-u.ac.jp>
2022-02-11 10:04:10 +0900

    save csv

--- SensorLogs/Views/Logs/LogsView.swift
+++ SensorLogs/Views/Logs/LogsView.swift
@@ -8,49 +8,75 @@ struct LogsView: View {
     @ObservedObject var apmanager = AtmosphericPressureManager()
     @ObservedObject var sbmanager = ScreenBrightnessManager()
     
-    @State var currentdatetime = GetCurrentDatetime()
+    @State var recordflag: Bool = true
     @State var timer :Timer?
+    @State var currentdatetime = GetCurrentDatetime()
+    
+    var currentdate = GetCurrentDate()
     
     let relavailabe = CMAltimeter.isRelativeAltitudeAvailable()
     let absavailabe = CMAltimeter.isAbsoluteAltitudeAvailable()
     
-    var path:URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("sample.csv")
+    var path:URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("\(GetCurrentDatetimeFilename())_SensorLogs.csv")
+    
+    let logswriter = LogsWriter()
 
     var body: some View {
         VStack(spacing: 30) {
             VStack(spacing: 30) {
                 Text(currentdatetime)
-                Text(relavailabe ? apmanager.pressureString : "----")
-                Text(absavailabe ? apmanager.absaltitudeString : "----")
-                Text(sbmanager.brightnessString != "" ? sbmanager.brightnessString : "----")
+                Text(relavailabe ? apmanager.pressureString : "気圧:----")
+                Text(absavailabe ? apmanager.absaltitudeString : "高度:----")
+                Text(sbmanager.brightnessString != "" ? sbmanager.brightnessString : "輝度:----")
+            }
+            if recordflag {
+                Button(action: {
+                    recordflag = false
+                    print("path is \(path)")
+                    print("logswriter.open")
+                    self.logswriter.open(path)
+                    self.timer = Timer.scheduledTimer(withTimeInterval: 15.0, repeats: true) {_ in
+                        let logs:[String: String] = [
+                            "datetime": "\(currentdatetime)",
+                            "atmospheric_pressure": "\(apmanager.pressureString)",
+                            "abs_altitude": "\(apmanager.absaltitudeString)",
+                            "screen_brightness": "\(sbmanager.brightnessString)"
+                        ]
+                        print("logswriter.write")
+                        self.logswriter.write(logs)
+                    }
+                }) {
+                    Text("Start Recording")
+                }
+            } else {
+                Button(action: {
+                    recordflag = true
+                    print("logswriter.close")
+                    self.timer?.invalidate()
+                    let logs:[String: String] = [
+                        "datetime": "\(currentdatetime)",
+                        "atmospheric_pressure": "\(apmanager.pressureString)",
+                        "abs_altitude": "\(apmanager.absaltitudeString)",
+                        "screen_brightness": "\(sbmanager.brightnessString)"
+                    ]
+                    print("logswriter.write")
+                    self.logswriter.write(logs)
+                    self.logswriter.close()
+                }) {
+                    Text("Stop Recording")
+                }
             }
             Button(action: {
                 allReset()
             }) {
                 Text("Reset")
             }
-            Button(action: {
-                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-                    self.currentdatetime = GetCurrentDatetime()
-                    sbmanager.doReset()
-                }
-            }) {
-                Text("Start Recording")
+        }.onAppear(perform: {
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                self.currentdatetime = GetCurrentDatetime()
+                sbmanager.doReset()
             }
-            Button(action: {
-                let logs:[String: String] = [
-                    "datetime": "\(currentdatetime)",
-                    "atmospheric_pressure": "\(apmanager.pressureString)",
-                    "abs_altitude": "\(apmanager.absaltitudeString)",
-                    "screen_brightness": "\(sbmanager.brightnessString)"
-                ]
-                print(path)
-                ExecLogsWriter(path: path, logs: logs)
-                
-            }) {
-                Text("Save")
-            }
-        }
+        })
     }
     
     func allReset() {
